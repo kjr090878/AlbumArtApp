8b19f186bc8ef8ad9db7bc53a291937f024b8e48
Kelly Robinson <kjr090878@yahoo.com>
2015-10-13 17:43:22 -0400

    AlbumArtApp

---
+++ AlbumArt/AlbumArt/TrackCell.swift
@@ -0,0 +1,24 @@
+//
+//  TrackCell.swift
+//  AlbumArt
+//
+//  Created by Kelly Robinson on 10/13/15.
+//  Copyright © 2015 Kelly Robinson. All rights reserved.
+//
+
+import UIKit
+
+class TrackCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
